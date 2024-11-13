b7bd4bb50ddf82d5e11867a3c47617f0ee1e85d3
Zaid <zaidk9620@gmail.com>
2024-11-13 21:06:31 +0530

    removed error

--- src/main/java/com/zaid/journalApp/entity/Journal.java
+++ src/main/java/com/zaid/journalApp/entity/Journal.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
-@Document(collection="journalEntries")
+@Document(collection = "journalEntries")
 @Data
 public class Journal {
     @Id
