bb6d6339b4ff938c97f9aba8fd45644171db1b35
Zaid <zaidk9620@gmail.com>
2024-11-27 10:15:54 +0530

    Added responseEntity in userController

--- src/main/java/com/zaid/journalApp/controller/UserController.java
+++ src/main/java/com/zaid/journalApp/controller/UserController.java
@@ -3,6 +3,8 @@ package com.zaid.journalApp.controller;
 import com.zaid.journalApp.entity.User;
 import com.zaid.journalApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,30 +17,48 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public List<User> getAllUsers(){
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user){
-        return userService.saveEntry(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        try {
+            User createdUser = userService.saveEntry(user);
+            return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PutMapping("/username/{username}")
-    public User updateUser(@PathVariable String username, @RequestBody User user){
+    public ResponseEntity<User> updateUser(@PathVariable String username, @RequestBody User user) {
         if (user == null || user.getUsername().isEmpty() || user.getPassword().isEmpty()) {
-            return null;
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return userService.updateUser(username, user);
+        User updatedUser = userService.updateUser(username, user);
+        if (updatedUser != null) {
+            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/username/{username}")
-    public boolean deleteUser(@PathVariable String username){
-        return userService.deleteUser(username);
+    public ResponseEntity<Void> deleteUser(@PathVariable String username) {
+        boolean isDeleted = userService.deleteUser(username);
+        if (isDeleted) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @GetMapping("/username/{username}")
-    public User getUser(@PathVariable String username){
-        return userService.getUser(username);
+    public ResponseEntity<User> getUser(@PathVariable String username) {
+        User user = userService.getUser(username);
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
